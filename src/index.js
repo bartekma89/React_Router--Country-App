0065803c72dee5b726ccd3a5e2cade5c55570c41
bartekma89 <bartekma89@gmail.com>
2018-04-15 12:49:00 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(document.getElementById('root'));
+registerServiceWorker();
