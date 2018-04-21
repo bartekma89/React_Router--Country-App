0d81f08dcbdbca7378519699bd386374931ec130
bartekma89 <bartekma89@gmail.com>
2018-04-22 00:54:06 +0200

    added style

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
+import './country.css';
 
 ReactDOM.render(
 	<Provider store={store}>
